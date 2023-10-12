c5be3529a1191eca42b8851f6707312fad7b780c
Urusha <urushadhikari@gmail.com>
2023-10-12 14:22:37 -0500

    Designed grids and added Icon and colors more to continue

--- my-portfolio/src/Pages/Education.tsx
+++ my-portfolio/src/Pages/Education.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import "../Styles/Education.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGraduationCap } from "@fortawesome/free-solid-svg-icons";
 
 export default function Education() {
   return (
-    <div>
-      <h1>Education</h1>
+    <div className="edContainer">
+      <h1 className="educationHeading">Education</h1>
+      <FontAwesomeIcon className="gradIcon" icon={faGraduationCap} size="2xl" />
+      <div className="educationContainer">
+        <div>
+          <h2>Associates</h2>
+          <section>
+            <img></img>
+          </section>
+        </div>
+        <div>
+          <h2>Bachelors</h2>
+          <section className="bachelors">
+            <img></img>
+          </section>
+        </div>
+        <div>
+          <h2>Certifications</h2>
+          <section>
+            <img></img>
+          </section>
+        </div>
+      </div>
     </div>
   );
 }
