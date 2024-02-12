99d1f5454c615c3e755bceec72d689be47548f52
Urusha <urushadhikari@gmail.com>
2024-02-12 12:51:39 -0600

    Updated all the stylings

--- my-portfolio/src/Components/ProjectContainer.tsx
+++ my-portfolio/src/Components/ProjectContainer.tsx
@@ -10,14 +10,8 @@ export default function ProjectContainer(props: any) {
   return (
     <div className="projectContainer">
       <div>
-        <h2>{props.projectTitle}</h2>
-        <img
-          alt="projectImg"
-          className="projectImg"
-          height="100px"
-          width="100px"
-          src={props.projectImg}
-        />
+        <h2 className="projectTitle">{props.projectTitle}</h2>
+        <img alt="projectImg" className="projectImg" src={props.projectImg} />
         <p className="projectDescription">{props.projectDescription}</p>
       </div>
     </div>
--- my-portfolio/src/Pages/Project.tsx
+++ my-portfolio/src/Pages/Project.tsx
@@ -2,69 +2,21 @@ import React from "react";
 import "../Styles/Project.css";
 import ProjectContainer from "../Components/ProjectContainer";
 import HeroPic from "../assets/images/HeroPic.JPG";
+import Project2 from "../assets/images/ProjectBM2.JPG";
+import Project1 from "../assets/images/ProjectPP.JPG";
 
 export default function Project() {
   return (
-    <div className="projectContainer">
+    <div className="projectsContainer">
       <ProjectContainer
-        projectTitle="My First Project"
-        projectImg={HeroPic}
-        projectDescription=" Hi, I am a developer based out of Dallas FortWorth metroplex area.
-            I am a Computer Science graduate with a Bachelor's degree, crafting
-            digital experiences one code at a time. | Design enthusiast with a
-            knack for pixel-perfection. | HTML, CSS, JS explorer on a mission to
-            create the web of tomorrow. | Turning ideas into interactive
-            realities. Welcome to my coding journey!"
+        projectTitle="BabyMama: E-commerce Website"
+        projectImg={Project2}
+        projectDescription="During a comprehensive full-stack bootcamp, I undertook the development of BabyMama, an e-commerce website using React.js for the frontend and Java for the backend. This project served as a culmination of my training, allowing me to apply a wide range of skills in frontend and backend development to create a fully functional online shopping platform."
       />
       <ProjectContainer
-        projectTitle="My Second Project"
-        projectImg={HeroPic}
-        projectDescription=" Hi, I am a developer based out of Dallas FortWorth metroplex area.
-           I am a Computer Science graduate with a Bachelor's degree, crafting
-           digital experiences one code at a time. | Design enthusiast with a
-           knack for pixel-perfection. | HTML, CSS, JS explorer on a mission to
-           create the web of tomorrow. | Turning ideas into interactive
-           realities. Welcome to my coding journey!"
-      />
-      <ProjectContainer
-        projectTitle="My Third Project"
-        projectImg={HeroPic}
-        projectDescription=" Hi, I am a developer based out of Dallas FortWorth metroplex area.
-           I am a Computer Science graduate with a Bachelor's degree, crafting
-           digital experiences one code at a time. | Design enthusiast with a
-           knack for pixel-perfection. | HTML, CSS, JS explorer on a mission to
-           create the web of tomorrow. | Turning ideas into interactive
-           realities. Welcome to my coding journey!"
-      />
-      <ProjectContainer
-        projectTitle="My Fourth Project"
-        projectImg={HeroPic}
-        projectDescription=" Hi, I am a developer based out of Dallas FortWorth metroplex area.
-           I am a Computer Science graduate with a Bachelor's degree, crafting
-           digital experiences one code at a time. | Design enthusiast with a
-           knack for pixel-perfection. | HTML, CSS, JS explorer on a mission to
-           create the web of tomorrow. | Turning ideas into interactive
-           realities. Welcome to my coding journey!"
-      />
-      <ProjectContainer
-        projectTitle="My Fifth Project"
-        projectImg={HeroPic}
-        projectDescription=" Hi, I am a developer based out of Dallas FortWorth metroplex area.
-           I am a Computer Science graduate with a Bachelor's degree, crafting
-           digital experiences one code at a time. | Design enthusiast with a
-           knack for pixel-perfection. | HTML, CSS, JS explorer on a mission to
-           create the web of tomorrow. | Turning ideas into interactive
-           realities. Welcome to my coding journey!"
-      />
-      <ProjectContainer
-        projectTitle="My Sixth Project"
-        projectImg={HeroPic}
-        projectDescription=" Hi, I am a developer based out of Dallas FortWorth metroplex area.
-           I am a Computer Science graduate with a Bachelor's degree, crafting
-           digital experiences one code at a time. | Design enthusiast with a
-           knack for pixel-perfection. | HTML, CSS, JS explorer on a mission to
-           create the web of tomorrow. | Turning ideas into interactive
-           realities. Welcome to my coding journey!"
+        projectTitle="Professional Portfolio"
+        projectImg={Project1}
+        projectDescription=" As part of my journey in web development, I created my professional portfolio website to showcase my projects, skills, and experiences. This website serves as a central hub for visitors to learn more about me, explore my projects, and connect with me professionally."
       />
     </div>
   );
